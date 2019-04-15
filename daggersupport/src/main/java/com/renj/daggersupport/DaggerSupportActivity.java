59099e51e6da7fd98555b909814ee50f796adfd1
Renj <itrenjunhua@163.com>
2019-04-15 11:33:24 +0800

    抽取 rxDagger 支持库代码

---
+++ daggersupport/src/main/java/com/renj/daggersupport/DaggerSupportActivity.java
@@ -0,0 +1,51 @@
+package com.renj.daggersupport;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.renj.rxsupport.rxview.RxBaseActivity;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasFragmentInjector;
+import dagger.android.support.HasSupportFragmentInjector;
+
+/**
+ * ======================================================================
+ * <p>
+ * 作者：Renj
+ * 邮箱：renjunhua@anlovek.com
+ * <p>
+ * 创建时间：2019-04-15   11:22
+ * <p>
+ * 描述：
+ * <p>
+ * 修订历史：
+ * <p>
+ * ======================================================================
+ */
+public abstract class DaggerSupportActivity extends RxBaseActivity implements HasFragmentInjector, HasSupportFragmentInjector {
+    @Inject
+    DispatchingAndroidInjector<Fragment> supportFragmentInjector;
+    @Inject
+    DispatchingAndroidInjector<android.app.Fragment> frameworkFragmentInjector;
+
+    @Override
+    public AndroidInjector<Fragment> supportFragmentInjector() {
+        return supportFragmentInjector;
+    }
+
+    @Override
+    public AndroidInjector<android.app.Fragment> fragmentInjector() {
+        return frameworkFragmentInjector;
+    }
+
+    @Override
+    protected void beforeSuper(Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
+    }
+}
--- mvpbase/src/main/java/com/renj/mvpbase/view/BaseActivity.java
+++ mvpbase/src/main/java/com/renj/mvpbase/view/BaseActivity.java
@@ -69,6 +69,7 @@ public abstract class BaseActivity extends SupportActivity implements IBaseView,
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        beforeSuper(savedInstanceState);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
         ActivityManager.addActivity(this);
@@ -82,6 +83,8 @@ public abstract class BaseActivity extends SupportActivity implements IBaseView,
         initPresenter();
         initData();
     }
+
+    protected void beforeSuper(Bundle savedInstanceState){}
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
