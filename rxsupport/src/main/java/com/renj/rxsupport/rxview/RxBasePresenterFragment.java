4e7bfbc4b44e0a11b8731bae4e8802843e6f25ef
Renj <itrenjunhua@163.com>
2019-04-15 11:13:40 +0800

    抽取 rxJava 支持库代码

---
+++ rxsupport/src/main/java/com/renj/rxsupport/rxview/RxBasePresenterFragment.java
@@ -0,0 +1,45 @@
+package com.renj.rxsupport.rxview;
+
+import android.support.annotation.NonNull;
+
+import com.renj.mvpbase.presenter.BasePresenter;
+import com.renj.mvpbase.view.BasePresenterFragment;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * ======================================================================
+ * 作者：Renj
+ * <p>
+ * 创建时间：2017-05-12   10:22
+ * <p>
+ * 描述：MVP 模式中基于 RxJava 的 View，对生命周期进行绑定
+ * <p>
+ * 修订历史：
+ * <p>
+ * ======================================================================
+ */
+public abstract class RxBasePresenterFragment<T extends BasePresenter> extends BasePresenterFragment<T> {
+    protected CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    /**
+     * 将 {@link Disposable} 增加到 {@link CompositeDisposable} 中，对生命周期进行控制
+     *
+     * @param disposable 需要增加的 {@link Disposable}
+     */
+    public void addDisposable(@NonNull Disposable disposable) {
+        if (compositeDisposable == null)
+            compositeDisposable = new CompositeDisposable();
+        compositeDisposable.add(disposable);
+    }
+
+    @Override
+    public void onDestroy() {
+        // 清除事件
+        if (compositeDisposable != null)
+            compositeDisposable.clear();
+
+        super.onDestroy();
+    }
+}
