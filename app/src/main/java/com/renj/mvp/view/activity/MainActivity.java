1d9f858c5c69c59a59610390095ed4458d4cdd08
Renj <itrenjunhua@163.com>
2019-07-07 01:50:01 +0800

    icon替换，资源文件修改

--- app/src/main/java/com/renj/mvp/view/activity/MainActivity.java
+++ app/src/main/java/com/renj/mvp/view/activity/MainActivity.java
@@ -9,6 +9,7 @@ import com.renj.common.utils.ResUtils;
 import com.renj.common.weight.MyViewPager;
 import com.renj.daggersupport.DaggerSupportActivity;
 import com.renj.mvp.R;
+import com.renj.mvp.utils.MyCommonUtils;
 import com.renj.mvp.view.fragment.HomeFragment;
 import com.renj.mvp.view.fragment.MyFragment;
 import com.renj.mvp.view.fragment.NewsFragment;
@@ -35,21 +36,24 @@ public class MainActivity extends DaggerSupportActivity {
 
     @Override
     public void initData() {
-        setPageBack(false,false,null);
+        setPageBack(false, false, null);
+        initTitles();
         initFragments();
-        setPageTitle(titles.get(0));
         initViewPager();
         initNavigationBar();
+    }
+
+    private void initTitles() {
+        titles.add(ResUtils.getString(R.string.main_tab_1));
+        titles.add(ResUtils.getString(R.string.main_tab_2));
+        titles.add(ResUtils.getString(R.string.main_tab_3));
+        setPageTitle(titles.get(0));
     }
 
     private void initFragments() {
         fragments.add(HomeFragment.Companion.newInstance());
         fragments.add(NewsFragment.Companion.newInstance());
         fragments.add(MyFragment.Companion.newInstance());
-
-        titles.add(ResUtils.getString(R.string.main_tab_1));
-        titles.add(ResUtils.getString(R.string.main_tab_2));
-        titles.add(ResUtils.getString(R.string.main_tab_3));
     }
 
     private void initViewPager() {
@@ -69,18 +73,18 @@ public class MainActivity extends DaggerSupportActivity {
     private void initNavigationBar() {
         bottomTab
                 .setMode(BottomNavigationBar.MODE_FIXED)
-                .addItem(new BottomNavigationItem(R.mipmap.new1_s, titles.get(0))
-                        .setInactiveIconResource(R.mipmap.new1_n)
-                        .setActiveColor(0xFFFF5521)
-                        .setInActiveColor(0xFF333333))
-                .addItem(new BottomNavigationItem(R.mipmap.new2_s, titles.get(1))
-                        .setInactiveIconResource(R.mipmap.new2_n)
-                        .setActiveColor(0xFFFF5521)
-                        .setInActiveColor(0xFF333333))
-                .addItem(new BottomNavigationItem(R.mipmap.my_s, titles.get(2))
-                        .setInactiveIconResource(R.mipmap.my_n)
-                        .setActiveColor(0xFFFF5521)
-                        .setInActiveColor(0xFF333333))
+                .addItem(new BottomNavigationItem(R.mipmap.home_s, titles.get(0))
+                        .setInactiveIconResource(R.mipmap.home_n)
+                        .setActiveColor(ResUtils.getColor(R.color.app_main))
+                        .setInActiveColor(ResUtils.getColor(R.color.color_gray)))
+                .addItem(new BottomNavigationItem(R.mipmap.found_s, titles.get(1))
+                        .setInactiveIconResource(R.mipmap.found_n)
+                        .setActiveColor(ResUtils.getColor(R.color.app_main))
+                        .setInActiveColor(ResUtils.getColor(R.color.color_gray)))
+                .addItem(new BottomNavigationItem(R.mipmap.me_s, titles.get(2))
+                        .setInactiveIconResource(R.mipmap.me_n)
+                        .setActiveColor(ResUtils.getColor(R.color.app_main))
+                        .setInActiveColor(ResUtils.getColor(R.color.color_gray)))
                 .initialise();
 
         bottomTab.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
@@ -100,5 +104,8 @@ public class MainActivity extends DaggerSupportActivity {
 
             }
         });
+
+        // 放在后面才能生效
+        MyCommonUtils.setBottomNavigationItem(bottomTab,6,24,12);
     }
 }
